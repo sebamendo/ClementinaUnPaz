78fae5aa5ab0e5d1c9149f22f1671d576095d22e
sebamendo <109764839+sebamendo@users.noreply.github.com>
2024-10-21 20:05:40 -0300

    Add files via upload
    primer boceto de la web del centro de estudiantes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.botton-inicio-a').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
